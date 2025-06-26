c88d36df0fd298ec6ce60e182cc1f93f48acd889
Silvio <silviosisa@gmail.com>
2025-06-26 07:26:45 -0300

    Prime numbers tests

---
+++ src/tests/primeFactors.tests.ts
@@ -0,0 +1,20 @@
+/*
+2 -> [2]
+2 * 2 -> [2,2]
+2 * 2 * 2 -> [2,2,2]
+3 -> [3]
+3 * 3 -> [3,3]
+3 * 2 -> [2,3]
+5 * 5 -> [5,5]
+5 * 7 * 11 * 3 -> [3,5,7,11]
+*/
+
+function getPrimeFactorsFor(number: number) {
+    return null;
+}
+
+describe('The prime factors', () => {
+    it('finds the prime composition of the given number', () => {
+        expect(getPrimeFactorsFor(2)).toEqual([2])
+    })
+})
