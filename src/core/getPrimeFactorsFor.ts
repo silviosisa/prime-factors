0a43aaf28229257678b003b81c46fece6e9aa7f7
Silvio <silviosisa@gmail.com>
2025-06-27 06:01:07 -0300

    refactor: reorganize test

---
+++ src/core/getPrimeFactorsFor.ts
@@ -0,0 +1,12 @@
+export function getPrimeFactorsFor(number: number) {
+    let factor = 2;
+    while (number % factor !== 0) {
+        ++factor;
+    }
+    const factors = [factor];
+    const remainder = number / factor;
+    if (remainder > 1) {
+        return factors.concat(getPrimeFactorsFor(remainder));
+    }
+    return factors;
+}
--- src/tests/primeFactors.test.ts
+++ src/tests/primeFactors.test.ts
@@ -1,36 +1,20 @@
-/*
-2 -> [2]
-2 * 2 -> [2,2]
-2 * 2 * 2 -> [2,2,2]
-3 -> [3]
-3 * 3 -> [3,3]
-3 * 2 -> [2,3]
-5 * 5 -> [5,5]
-5 * 7 * 11 * 3 -> [3,5,7,11]
-*/
-
-function getPrimeFactorsFor(number: number) {
-    let factor = 2;
-    while(number % factor !== 0) {
-        ++factor;
-    }
-    const factors = [factor];
-    const remainder = number / factor;
-    if (remainder > 1) {
-        return factors.concat(getPrimeFactorsFor(remainder));
-    }
-    return factors;
-}
+import {getPrimeFactorsFor} from "../core/getPrimeFactorsFor";
 
 describe('The prime factors', () => {
     it('finds the prime composition of the given number', () => {
-        expect(getPrimeFactorsFor(2)).toEqual([2])
         expect(getPrimeFactorsFor(2*2)).toEqual([2, 2])
-        expect(getPrimeFactorsFor(2*2*2)).toEqual([2, 2, 2])
-        expect(getPrimeFactorsFor(3)).toEqual([3])
         expect(getPrimeFactorsFor(3*3)).toEqual([3, 3])
         expect(getPrimeFactorsFor(3*2)).toEqual([2, 3])
         expect(getPrimeFactorsFor(5*5)).toEqual([5, 5])
+    })
+    it('knows whtat is a prime number', () => {
+        expect(getPrimeFactorsFor(2)).toEqual([2])
+        expect(getPrimeFactorsFor(3)).toEqual([3])
+    })
+    it('produces the same result to multiply the number in the output list',  ()=> {
+        expect(getPrimeFactorsFor(2*2*2)).toEqual([2, 2, 2])
+    })
+    it('orders the prime factors from the smallest to the biggest', () => {
         expect(getPrimeFactorsFor(5*7*11*3)).toEqual([3, 5, 7, 11])
     })
 })
