17d71d4dffa8c66b38ef1d274ce474baf10176e6
Silvio <silviosisa@gmail.com>
2025-06-27 06:27:47 -0300

    refactor: replace condition by ternary, extract method, rename variable

--- src/core/getPrimeFactorsFor.ts
+++ src/core/getPrimeFactorsFor.ts
@@ -1,11 +1,13 @@
-export function getPrimeFactorsFor(number: number) {
+const findSmallestPrime = (number: number) => {
     let factor = 2;
     while (number % factor !== 0) {
         ++factor;
     }
-    const remainder = number / factor;
-    if (remainder <= 1) {
-        return [factor];
-    }
-    return [factor].concat(getPrimeFactorsFor(remainder));
+    return factor;
+}
+
+export function getPrimeFactorsFor(number: number) {
+    let prime = findSmallestPrime(number);
+    const remainder = number / prime;
+    return remainder <= 1 ? [prime] : [prime].concat(getPrimeFactorsFor(remainder));
 }
