882e65fcb33ab472ddb125d7ca65d6d3ce664523
Silvio <silviosisa@gmail.com>
2025-06-27 05:34:17 -0300

    red finds the prime composition of the given number

--- src/tests/primeFactors.test.ts
+++ src/tests/primeFactors.test.ts
@@ -23,5 +23,6 @@ describe('The prime factors', () => {
     it('finds the prime composition of the given number', () => {
         expect(getPrimeFactorsFor(2)).toEqual([2])
         expect(getPrimeFactorsFor(2*2)).toEqual([2, 2])
+        expect(getPrimeFactorsFor(2*2*2)).toEqual([2, 2, 2])
     })
 })
